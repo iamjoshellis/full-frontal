d9127531059c5ae26e85bc846802080b14f8698e
Josh Ellis <iamjoshellis@gmail.com>
2016-08-13 17:03:20 +0100

    Added some basic form validation ✅

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,41 @@
 (function($) {
 
+  $.fn.isValid = function() {
+    return this[0].checkValidity();
+  };
+
+  var forms = {
+    $inputs : $('input, textarea'),
+    stateTouched : 'js-state--touched',
+    stateFilled : 'js-state--filled',
+    stateValid : 'js-state--valid',
+    stateInvalid : 'js-state--invalid',
+    setValidState : function($input) {
+      if ($input.isValid()) {
+        $input.removeClass(this.stateInvalid);
+        $input.addClass(this.stateValid);
+      } else {
+        $input.removeClass(this.stateValid);
+        $input.addClass(this.stateInvalid);
+      }
+    },
+    setEmptyState : function($input) {
+      if ($input.val()) {
+        $input.addClass(this.stateFilled);
+      } else {
+        $input.removeClass(this.stateFilled);
+      }
+    }
+  };
+
+  forms.$inputs.focusout(function() {
+    $(this).addClass(forms.stateTouched);
+    forms.touchedInputs = $(forms.stateTouched);
+  });
+
+  forms.$inputs.keyup(function() {
+    forms.setEmptyState($(this));
+    forms.setValidState($(this));
+  });
+
 })(jQuery);
